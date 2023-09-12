a406b4e53aab58116c7342d6a649b327c3539541
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-09-12 09:33:16 +0300

    Update shiur1.java

--- shiur1.java
+++ shiur1.java
@@ -1,1 +1,12 @@
+//1. 
+public class A
+{
+    public static void main(String []args)
+    {
+       int x=1,y=2,z=3;
+       System.out.println(x);
+       System.out.println(y);
+       System.out.println(z);
 
+    }
+}
