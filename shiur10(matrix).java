228f81e2fb40636f7a506fc829465a17c5560a9d
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-10-31 20:19:22 +0200

    Update shiur10(matrix).java

--- shiur10(matrix).java
+++ shiur10(matrix).java
@@ -16,3 +16,23 @@ public void f1(int rows,int columns)
             System.out.println();
         }
 }
+public void f2(int rows,int columns)
+{
+        Scanner scan=new Scanner(System.in);
+        int[][] arr2=new int[rows][columns];
+        int i,j;
+        for(i=0;i<arr2.length;i++)
+        {
+            for(j=0;j<arr2[i].length;j++)
+            {
+                System.out.println("Enter number between 0-100:");
+                arr2[i][j] = scan.nextInt();
+            }
+        }
+        for(i=0;i<arr2.length;i++)
+        {
+            for(j=0;j<arr2[i].length;j++)
+                System.out.print(arr2[i][j]+" ");
+            System.out.println();
+        }
+}
