7f06966523a9a1c539172e703fa138a6ee55ee97
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-11-05 19:24:50 +0200

    Update shiur10(matrix).java

--- shiur10(matrix).java
+++ shiur10(matrix).java
@@ -17,11 +17,11 @@ public void f1(int rows,int columns)
         }
 }
 //2.
-public void f2(int rows,int columns)
+public void f2()
 {
         Scanner scan=new Scanner(System.in);
-        int[][] arr2=new int[rows][columns];
-        int i,j;
+        int[][] arr2=new int[3][3];
+        int i,j,maxLine=0;
         for(i=0;i<arr2.length;i++)
         {
             for(j=0;j<arr2[i].length;j++)
@@ -32,8 +32,15 @@ public void f2(int rows,int columns)
         }
         for(i=0;i<arr2.length;i++)
         {
+            int sumLine=0;
             for(j=0;j<arr2[i].length;j++)
+            {
                 System.out.print(arr2[i][j]+" ");
+                sumLine = sumLine + arr2[i][j];
+            }
             System.out.println();
+            if(sumLine>maxLine)
+                maxLine=sumLine;
         }
+        System.out.println(maxLine);
 }
