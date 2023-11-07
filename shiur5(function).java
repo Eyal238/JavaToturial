f1f2b3bdd3c84712cbd36813fa00b97a338009de
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-11-07 13:54:28 +0200

    Update shiur5(function).java

--- shiur5(function).java
+++ shiur5(function).java
@@ -4,3 +4,8 @@ public void f2(int num)
    int complete=num/5;
    System.out.println(complete);
 }
+public void f3(int num1,int num2)
+{
+    int duple=num1*num2;
+    System.out.println(duple);
+}
