9d76ba029436a74bf3e97890e44427985a99d43e
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-12-17 08:42:57 +0200

    Update shiur12.java

--- shiur12.java
+++ shiur12.java
@@ -1,1 +1,9 @@
-
+public int recMin(int[] arr,int i)
+{
+        if(i==arr.length-1)
+            return arr[arr.length-1];
+        int minInd=recMin(arr,i+1);
+        if(arr[i]>arr[minInd])
+            return minInd;
+        else return i;
+}
