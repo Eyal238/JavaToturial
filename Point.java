0d520da1a91e6e92d1baac228e2f42c3c2e7a3ad
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-12-20 14:56:01 +0200

    Update Point.java

--- Point.java
+++ Point.java
@@ -6,6 +6,28 @@ public class Point
         _x=x;
         _y=y;
     }
+    // בנאי העתקה
+    public Point(Point other )
+    {
+        _x=other._x;
+        _y=other._y;
+    }
+    public int getX()
+    {
+        return _x;
+    }
+    public int getY()
+    {
+        return _y;
+    }
+    public void setX(int x)
+    {
+        _x=x;
+    }
+    public void setY(int y)
+    {
+        _y=y;
+    }
     public void move(int deltaX,int deltaY)
     {
         _x=_x+deltaX;
@@ -14,5 +36,12 @@ public class Point
     public void printStatus()
     {
         System.out.println("("+_x+","+_y+")");
+    }
+    public boolean equal(Point other)
+    {
+        if((_x==other._x)&&(_y==other._y))
+            return true;
+        else
+            return false;
     }
 }
