b6ee9c452a05d77067ae6bc6ea362dcd68296ba7
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-12-29 14:28:25 +0200

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,40 +1,75 @@
-public class Circle
+ublic class Circle 
 {
     private double _radius;
     private Point _center;
-    public Circle(int x,int y,double radius)
+
+    public Circle(int x, int y, double radius) 
     {
-        _center=new Point(x,y);
-        if(radius>0)
-            _radius=radius;
+        _center = new Point(x, y);
+        if (radius > 0)
+            _radius = radius;
         else
-            _radius=1;
+            _radius = 1;
     }
-    public Circle(Point center,double radius)
+
+    public Circle(Point center, double radius) 
     {
-        _center=new Point(center); //copy constructor
-        if(radius>0)
-            _radius=radius;
+        _center = new Point(center); //copy constructor
+        if (radius > 0)
+            _radius = radius;
     }
-    public Circle(Circle other)
+
+    public Circle(Circle other) 
     {
-        _center=new Point(other._center);
-        _radius=other._radius;
+        _center = new Point(other._center);
+        _radius = other._radius;
     }
-    public Point getCenter()
+
+    public Point getCenter() 
     {
         return new Point(_center);
     }
-    public double getRadius()
+
+    public double getRadius() 
     {
         return _radius;
     }
-    public void setCenter(Point center)
+
+    public void setCenter(Point center) 
     {
-        _center=new Point(center);
+        _center = new Point(center);
     }
-    public void setRadius(double radius)
+
+    public void setRadius(double radius) 
     {
-        _radius=radius;
+        _radius = radius;
+    }
+
+    public double primeter() 
+    {
+        return (2 * Math.PI * _radius);
+    }
+
+    public double area() 
+    {
+        return (Math.PI * Math.pow(_radius, 2));
+    }
+
+    public void move(int deltaX, int deltaY)
+    {
+        _center.move(deltaX,deltaY);
+    }
+    public boolean equals(Circle other)
+    {
+        if(_center.equal(other._center)&&_radius== other._radius)
+            return true;
+        else return false;
+    }
+    //  ניתן גם להשוות בין רדיוסים
+    public boolean isLarger(Circle other)
+    {
+        if(this.area()>other.area())
+            return true;
+        else return false;
     }
 }
