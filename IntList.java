0ac32a88c13314e27b3b4980281a2e5a8bb40675
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2024-01-17 20:27:28 +0200

    Update IntList.java

--- IntList.java
+++ IntList.java
@@ -1,1 +1,42 @@
+public class IntList
+{
+    private IntNode head;
+    public IntList()
+    {
+        head=null;
+    }
+    public IntList(IntNode node)
+    {
+        head=node;
+    }
+    public boolean empty()
+    {
+        if(head==null)
+            return true;
+        else
+            return false;
+    }
+    // מחזיר את האיבר הבא ברשימה
+    public IntNode nextElement(IntNode node)
+    {
+        return node.getNext();
+    }
+    public int getValueOfNode(IntNode node)
+    {
+        return node.getValue();
+    }
+    //מוסיפה איבר לסוף הרשימה
+   public void addToEnd(IntNode node)
+   {
+       if(empty())
+           head=node;
+       else
+       {
+           IntNode ptr=head;
+           while(ptr.getNext()!=null)
+               ptr=ptr.getNext();
+           ptr.setNext(node);
+       }
+   }
+}
 
