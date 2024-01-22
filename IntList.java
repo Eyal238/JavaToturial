fd82e3463b69d2d88c14ce0cdc4fb1cfec9cb938
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2024-01-22 20:58:08 +0200

    Update IntList.java

--- IntList.java
+++ IntList.java
@@ -9,6 +9,7 @@ public class IntList
     {
         head=node;
     }
+    // האם הרשימה ריקה?
     public boolean empty()
     {
         if(head==null)
@@ -16,19 +17,57 @@ public class IntList
         else
             return false;
     }
-    // מחזיר את האיבר הבא ברשימה
+    //מחזירה את אורך הרשימה
+    public int length()
+    {
+        IntNode ptr=head;
+        int count=0;
+        while(ptr!=null)
+        {
+            count++;
+            ptr=ptr.getNext();
+        }
+        return count;
+    }
+    // מחזירה את האיבר הבא ברשימה
     public IntNode nextElement(IntNode node)
     {
         return node.getNext();
     }
+    // מחזיר את הערך של האיבר הנוכחי
     public int getValueOfNode(IntNode node)
     {
         return node.getValue();
     }
+    //מחזירה את האיבר הקודם
+    public IntNode predecessor(IntNode node)
+    {
+        if(head==null||head==node)  // אם הרשימה ריקה או הוא שווה לראש הרשימה ואין איבר לפניו
+            return null;
+        IntNode ptr=head;
+        while(ptr.getNext()!=null)    //אם האיבר הבא הוא אז נחזיר את הנוכחי
+        {
+            if(ptr.getNext()==node)
+                return ptr;
+            else ptr=ptr.getNext();
+        }
+        return null;
+    }
+    // מדפיסה את הרשימה
+    public void printList()
+    {
+        IntNode temp=head;
+        while(temp!=null)
+        {
+            System.out.print(temp.getValue()+"-->");
+            temp=temp.getNext();
+        }
+        System.out.println("null");
+    }
     //מוסיפה איבר לסוף הרשימה
    public void addToEnd(IntNode node)
    {
-       if(empty())
+       if(empty()==true)
            head=node;
        else
        {
@@ -38,5 +77,33 @@ public class IntList
            ptr.setNext(node);
        }
    }
+   // הוספת איבר לתחילת הרשימה
+   public void addToHead(IntNode node)
+   {
+       IntNode temp=head;
+       head=node;
+       node.setNext(temp);
+   }
+   // הוצאת איבר מרשימה
+   public void remove(int num)
+   {
+       if(head!=null)
+       {
+           if(head.getValue()==num)
+               head=head.getNext();
+           else
+           {
+               IntNode ptr = head;
+               while (ptr.getNext() != null)
+               {
+                   if(ptr.getNext().getValue()==num)
+                        ptr.setNext(ptr.getNext().getNext());
+                   else
+                        ptr=ptr.getNext();
+               }
+           }
+       }
+   }
 }
 
+   
