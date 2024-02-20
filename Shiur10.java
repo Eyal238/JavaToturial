2aaaf13b398fe7540ed04ad2e3cebcf8ac4c8232
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2024-02-20 10:35:45 +0200

    Update Shiur10.java

--- Shiur10.java
+++ Shiur10.java
@@ -11,3 +11,23 @@ public void f1(int[] arr,int index)
      else
          System.out.println("Error!");
 }
+//8.
+public int f8(int[] arr)
+{
+        int i,sumTotal=0,sumCurrent=0;
+        for(i=0;i<arr.length;i++)
+            sumTotal=sumTotal+arr[i];
+        for(i=0;i<arr.length-1;i++)
+        {
+            if((arr[i]+sumCurrent)==(sumTotal-(arr[i]+arr[i+1])))
+                return (i+1);
+            else
+            {
+                sumTotal=sumTotal-arr[i];
+                sumCurrent+=arr[i];
+            }
+
+
+        }
+        return -1;
+}
