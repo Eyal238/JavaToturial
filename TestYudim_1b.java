e776290b5bf4cd4f7335285963c1d4492f360e6d
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2024-03-09 21:30:11 +0200

    Update TestYudim_1b.java

--- TestYudim_1b.java
+++ TestYudim_1b.java
@@ -1,1 +1,21 @@
-
+import java.util.Scanner;
+import java.util.Random;
+public class Test1b
+{
+    Scanner scan=new Scanner(System.in);
+    Random r=new Random();
+    public void f1()
+    {
+        double total;
+        System.out.println("Enter price 1:");
+        double price1=scan.nextDouble();
+        System.out.println("Enter price 1:");
+        double price2=scan.nextDouble();
+        if(price1>=price2)
+            total=(price2/2)+price1;
+        else
+            total=(price1/2)+price2;
+        System.out.println(total);
+    }
+  
+}
