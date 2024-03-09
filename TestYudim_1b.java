07567164d8ceb14bab978921625c90494847f786
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2024-03-09 21:41:42 +0200

    Update TestYudim_1b.java

--- TestYudim_1b.java
+++ TestYudim_1b.java
@@ -17,5 +17,19 @@ public class Test1b
             total=(price1/2)+price2;
         System.out.println(total);
     }
-  
+  public void f2()
+  {
+        System.out.println("Enter name1:");
+        String name1=scan.next();
+        System.out.println("Enter name2:");
+        String name2=scan.next();
+        int num1= r.nextInt(10);
+        int num2=r.nextInt(10);
+        if(((num1>=num2)&&(name1.compareTo(name2)<0))||((num2>num1)&&(name2.compareTo(name1)<0)))
+            System.out.println(name1);
+        else
+            System.out.println("Nobody");
+        System.out.println("num1="+num1);
+        System.out.println("num2="+num2);
+  }
 }
